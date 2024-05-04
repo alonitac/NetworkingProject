7d62b20f1a8bac5f58535d3ee4342c6ddbd75b55
gatmbarz123 <bar30301@gmail.com>
2024-05-04 19:49:32 +0300

    my first commit

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
 
+##if [ $# -ne 2 ]; then
+    ##echo "Usage: $0 <public_ip> <private_ip>"
+   ## exit 1
+##fi
 
-ssh -i $KEY_PATH ubuntu@$1  ssh -i new-key.pem ubuntu@$2 
+if [ $# -eq 1 ]; then
+   ssh -i $KEY_PATH ubuntu@$1
+   exit 1
+fi
 
- 
+
+if [ $# -eq 3 ]; then 
+   ssh -i $KEY_PATH -N -L 2222:$2:22 ubuntu@"$1" &
+   sleep 5
+   ssh -i $KEY_PATH -p 2222 ubuntu@localhost $3
+   exit 2
+fi
+
+if [ $# -eq 2 ]; then 
+   ssh -i $KEY_PATH -N -L 2222:$2:22 ubuntu@"$1" &
+   sleep 5
+   ssh -i $KEY_PATH -p 2222 ubuntu@localhost 
+   exit 3
+fi
+
+
+
+if [ -z $KEY_PATH ]; then
+   echo "KEY_PATH env var is expected"
+   exit 4
+fi
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <public_ip> <private_ip>"
+    exit 4
+fi
+
+
+##public_ip=$1
+##private_ip=$2
+
+##ssh -i $KEY_PATH -N -L 2222:$2:22 ubuntu@$1 &
+
+
+##sleep 5
+
+##ssh -i $KEY_PATH -p 2222 ubuntu@localhost
+
