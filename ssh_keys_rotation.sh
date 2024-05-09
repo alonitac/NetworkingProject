d69532cc47559840c24946a74f9c6e480ceda134
gatmbarz123 <bar30301@gmail.com>
2024-05-09 20:32:55 +0300

    NOW NOW WORK

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -5,11 +5,11 @@ cp key.pub old_key.pub
 chmod 600 old_key
 rm ~/key
 rm ~/key.pub
-ssh-keygen -f ~/key
+ssh-keygen -f ~/key -N "" ##no password
 sleep 5
 scp -i old_key -p key.pub ubuntu@$1:
 ssh -i old_key ubuntu@$1 cp ~/key.pub .ssh/authorized_keys
-
+exit 5
 
 
 
