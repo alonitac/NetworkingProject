0212704ccc7b6a9165c9f361da79cd16af3a96fe
keretdodor <keret.dor@gmail.com>
2024-05-25 20:28:13 +0300

    FOURTH-TRY

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if [ -z $1 ]; then
     echo "an IP is needed to excute the script"
     exit 1
@@ -10,12 +11,14 @@ cp newkey.pub oldkey.pub
 
 ssh-keygen -t rsa -b 4096 -f newkey -N ""
 
+chmod 600 ~/key.pem
 if scp -i ~/key.pem ~/newkey.pub ubuntu@$1:~; then
    ssh -i ~/key.pem ubuntu@$1 'cat ~/newkey.pub >> ~/.ssh/authorized_keys'
 else
   if [ $? -eq 1 ]; then
-    chmod u+x oldkey
+    chmod 600 oldkey
     scp -i ~/oldkey ~/newkey.pub ubuntu@$1:~ 
     ssh -i ~/oldkey ubuntu@$1 'cat ~/newkey.pub >> ~/.ssh/authorized_keys'
+    
   fi
 fi  
