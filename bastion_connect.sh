8bd802fba8f780c057600507d6b9be812459e8c4
amirmirs <amirshl10056@gmail.com>
2024-05-26 11:33:54 +0300

    bastion edit

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]; then
-   ssh -i $KEY_PATH ubuntu@$1 -t
-fi
-
-
-if [ $# -eq 2 ]; then
-
-  ssh -i $KEY_PATH ubuntu@$1 -t "ssh -i /home/ubuntu/key -t ubuntu@$2"
-  
-fi
-
-
-if [ $# -eq 3 ]; then
-
-   ssh -i $KEY_PATH ubuntu@$1 -t "ssh -i /home/ubuntu/key -t ubuntu@$2 $3"
-
-fi
-
 if [ -z $1 ]; then
    echo "Please provide bastion IP address"
    exit 5
@@ -27,3 +9,17 @@ if [ -z $KEY_PATH ]; then
   echo "KEY_PATH env var is expected"
   exit 5
 fi
+
+if [ -z $2 ]; then
+  ssh -i $KEY_PATH ubuntu@$1 -t
+
+fi
+
+
+if [ -z "$3" ]; then
+   ssh -i $KEY_PATH ubuntu@$1 -t "ssh -i /home/ubuntu/key -t ubuntu@$2"
+else
+   ssh -i $KEY_PATH ubuntu@$1 -t "ssh -i /home/ubuntu/key -t ubuntu@$2" "$3"
+fi
+
+
