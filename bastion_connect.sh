1b16d4fd3dbdeb6d51cfb99fcc15f3a42418b838
linoy <linoyedri18898@gmail.com>
2024-05-29 23:30:44 +0300

    added bastion connect and ec2_instances

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -9,12 +9,12 @@ fi
 
 if [ $# -eq 2 ]; then
     echo "case 1"
-    ssh -i $KEY_PATH ubuntu@$1 "ssh -i /home/ubuntu/private.pem ubuntu@$2"
+    ssh -i $KEY_PATH ubuntu@$1 "ssh -i /home/ubuntu/private.pem ec2-user@$2"
 elif [ $# -eq 1 ]; then
     echo "case 2"
     ssh -i $KEY_PATH ubuntu@$1
 elif [ $# -eq 3 ]; then
     echo "case 3"
-    ssh -i $KEY_PATH ubuntu@$1 "ssh -i /home/ubuntu/private.pem ubuntu@$2 $3"
+    ssh -i $KEY_PATH ubuntu@$1 "ssh -i /home/ubuntu/private.pem ec2-user@$2 $3"
 fi
 
